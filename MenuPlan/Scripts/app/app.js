e1214a182d08e0b6346b9a98a9a14377ffe96332
Kenneth Laforteza <kbl80_55@yahoo.com>
2018-04-20 16:58:11 +0800

    Code and Files Cleanup

--- MenuPlan/Scripts/app/app.js
+++ MenuPlan/Scripts/app/app.js
@@ -2,7 +2,6 @@
     'use strict';
 
     var systemx = angular.module('systemx', [
-        'systemx.home',
         'systemx.market',
         'ui.router'
     ]);
@@ -11,27 +10,6 @@
         function ($stateProvider, $locationProvider, $urlRouterProvider) {
             //$locationProvider.html5Mode(true);
             $urlRouterProvider.otherwise('/market');
-
-            //$stateProvider
-            //    .state('home', {
-            //        url: '/home',
-            //        views: {
-            //            "main": {
-            //                controller: 'homeCtrl',
-            //                templateUrl: 'Scripts/app/home/home.tpl.html',
-            //            }
-            //        }
-            //    });
-            //$stateProvider
-            //    .state('market', {
-            //    url: '/market',
-            //    views: {
-            //        "main": {
-            //            controller: 'marketCtrl',
-            //            templateUrl: 'Scripts/app/market/market.tpl.html',
-            //        }
-            //    }
-            //});
         }]);
 
     systemx.controller('AppController', ['$scope', '$state', function ($scope, $state) {
